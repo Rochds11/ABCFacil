201eb91c4048869a6ee59a1c44b33cacdfb1fe2b
LUISDDELAROCHA <sonyvaio11>
2015-05-03 22:44:35 -0600

    Se comento que el pancho dsds

--- app/src/main/java/mx/com/isdi/abcfacil/main.java
+++ app/src/main/java/mx/com/isdi/abcfacil/main.java
@@ -15,7 +15,7 @@ public class main extends ActionBarActivity {
     }
 
 
-    //El pancho
+    //El pancho dsads
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
